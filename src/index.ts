2898f54aacb65cf865945c4fe445e11a0b9ccded
Elivelton Alves <eliveltonsa92@gmail.com>
2023-04-07 19:40:23 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
+import router from "./routes/router";
 import express from "express";
 import cors from "cors";
-import router from "./routes/router";
 
 config();
 
