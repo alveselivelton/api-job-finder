2c6f9068a3e7037e3a6e56222706e58ecaa8894f
Elivelton Alves <eliveltonsa92@gmail.com>
2023-04-07 23:00:56 -0300

    update index.ts 3

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
 import { config } from "dotenv";
 import router from "./routes/router";
 import express from "express";
-import cors from "cors";
+// import cors from "cors";
 
 config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: true,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
-
-app.options(
-  "*",
-  cors({
-    origin: true,
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.use(express.json());
 
