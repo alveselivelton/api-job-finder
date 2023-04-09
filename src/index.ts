996a6f6666770d1cf1d728724facad1dcbd865ef
Elivelton Alves <eliveltonsa92@gmail.com>
2023-04-08 21:30:40 -0300

    update package.json and gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,21 @@
 import { config } from "dotenv";
 import router from "./routes/router";
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 
 config();
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 
 app.use("/api", router);
 
-import main from "../config/conn";
-
 const port = process.env.PORT;
+
+import main from "../config/conn";
 
 app.listen(port, () => {
   console.log(`API Working in port ${port}!`);
